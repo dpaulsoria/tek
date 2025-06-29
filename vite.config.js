8cafe297f44487fcd7ff97ae04d3252508f6055c
Paul Soria <dpaulsoria@gmail.com>
2025-06-28 22:44:02 -0500

    fix: corrigiendo vite config

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,25 @@
+// vite.config.js
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
+import vue     from '@vitejs/plugin-vue';
 
 export default defineConfig({
-    base: '',
-    plugins: [
-        laravel({
-            input: 'resources/js/app.js',
-            refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
-        }),
-    ],
+  base: '',  // ← forzar rutas relativas: /build/assets/… en vez de https://…/build/assets/…
+  plugins: [
+    laravel({
+      input: [
+        'resources/js/app.js',
+        "resources/js/Pages/{$page['component']}.vue",
+      ],
+      refresh: true,
+    }),
+    vue({
+      template: {
+        transformAssetUrls: {
+          base: null,
+          includeAbsolute: false,
+        },
+      },
+    }),
+  ],
 });
