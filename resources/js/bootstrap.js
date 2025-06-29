6af65553956effe8381ea662fc7894e090183650
Paul Soria <dpaulsoria@gmail.com>
2025-06-28 22:22:47 -0500

    fix: corrigiendo la configuracion de cors

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,7 +2,6 @@
 import axios from 'axios'
 
 
-axios.defaults.baseURL = import.meta.env.VITE_APP_URL || 'http://localhost:8000'
 axios.defaults.withCredentials = true
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
 axios.defaults.headers.common['Accept'] = 'application/json'
@@ -16,4 +15,4 @@ axios.defaults.headers.common['Accept'] = 'application/json'
   }
 })()
 // Exportar global
-window.axios = axios"SQLSTATE[HY000] [2002] Cannot connect to MySQL using SSL (Connection: mysql, SQL: select * from `sessions` where `id` = eTsnwhSa7Pxlgn85UvyCCgXuSSAsJuNnyZJDVZsx limit 1)"
+window.axios = axios
