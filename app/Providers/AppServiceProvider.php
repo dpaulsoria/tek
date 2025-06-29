453bce7576447c3a936b00022bbed0510c6802a0
Paul Soria <dpaulsoria@gmail.com>
2025-06-29 15:48:05 -0500

    fix: agregando validacion de produccion

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +22,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+        if ($this->app->environment('production')) {
+            // 1) Fuerza esquema HTTPS (para cualquier helper route(), asset(), url(), etc.)
+            URL::forceScheme('https');
+            // 2) Asegura que la raíz coincida con APP_URL (útil si estás detrás de proxy)
+            URL::forceRootUrl(config('app.url'));
+        }
     }
 }
