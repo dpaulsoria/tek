b74ce0f8d2f09dcea1d07345b2832df14c0185c3
Paul Soria <dpaulsoria@gmail.com>
2025-07-06 19:29:05 -0500

    fix: quitando migracion de script start

--- start.sh
+++ start.sh
@@ -6,7 +6,6 @@ mkdir -p storage/certs
 echo "$MYSQL_ATTR_SSL_CA_B64" | base64 -d > storage/certs/ca.pem
 
 # Migraciones y caches
-php artisan migrate --force
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
